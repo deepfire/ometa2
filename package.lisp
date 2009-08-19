2e07370f6e3ce10ddba85e6e11c75b2af5cd3671
John Leuner <jewel@subvert-the-dominant-paradigm.net>
2009-08-19 18:15:53 +0200

    fix previous bad commit

--- package.lisp
+++ package.lisp
@@ -1,5 +1,5 @@
 (defpackage #:ometa
   (:use :cl)
-  (:export :parse-grammar-file :compile-grammar-to-lisp :generate-parser :ometa :ometa-parser :ometa-compiler)
-  (:export :letter :digit :anything :exactly :ochar :ospace :spaces :token))
+  (:export :ometa :ometa-parser :ometa-compiler :parse-grammar :make-parser :ometa-input-stream :load-grammar :parse-input-file
+    :parse-input-string :letter :digit :anything :exactly :ochar :ospace :spaces :token))
 
