33652a20e969e725e40438bedb03ffcab01c1aad
Serge Kosyrev <deepfire@wintermute.(none)>
2010-11-03 14:18:15 +0300

    Allow control of generated parsers' optimisation quality

--- driver.lisp
+++ driver.lisp
@@ -28,23 +28,33 @@
 
 (defun parse-grammar (file-name)
     (let ((ometa (make-instance 'ometa-parser)))
-        (parse-input-file ometa file-name)
-        ))
+        (parse-input-file ometa file-name)))
 
-(defmacro with-compiled-forms (class-name grammar-file-name action)
+(defmacro with-compiled-forms (class-name grammar-file-name action &optional initial-action)
   `(let ((*ometa-compiler-target* ,class-name))
-    (loop for form in (mapcar #'ometa-compile (mapcar #'ometa-optimize (cdddr (parse-grammar ,grammar-file-name))) ) 
-        do (funcall ,action form)
-    )))
+     (let ((forms (mapcar #'ometa-compile (mapcar #'ometa-optimize (cdddr (parse-grammar ,grammar-file-name))))))
+       ,@(when initial-action
+               `((when forms
+                   (funcall ,initial-action))))
+       (loop for form in forms
+          do (funcall ,action form)))))
 
-(defun make-parser (class-name grammar-file-name parser-file-name )
+(defun make-parser (class-name grammar-file-name parser-file-name &key optimize-debug optimize-speed)
+  (let ((quality (cond ((and optimize-debug optimize-speed)
+                        (error "Conflicting optimization requirements."))
+                       (optimize-debug '(debug safety))
+                       (optimize-speed '(speed))
+                       (t              nil))))
     (with-open-file (stream parser-file-name :direction :output :if-exists :supersede)
-        (with-compiled-forms class-name grammar-file-name 
-            #'(lambda (form) 
-                (format stream "~%") 
-                (let ((*print-readably* t))
-                    (prin1 form stream)))
-                    )))
+      (with-compiled-forms class-name grammar-file-name 
+                           (lambda (form) 
+                             (format stream "~%") 
+                             (let ((*print-readably* t))
+                               (prin1 form stream)))
+                           (lambda ()
+                             (when quality
+                               (write `(declaim (optimize ,@quality)) :stream stream)))))))
 
 (defun load-grammar (class-name grammar-file-name)
-    (with-compiled-forms class-name grammar-file-name #'eval))
+    (with-compiled-forms class-name grammar-file-name
+      #'eval))
