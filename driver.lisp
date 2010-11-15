b5965b692aa08cd843f2264f961e7a75d4878b28
Serge Kosyrev <deepfire@wintermute.(none)>
2010-11-15 21:23:58 +0300

    Allow for generating output geared at specific package

--- driver.lisp
+++ driver.lisp
@@ -14,8 +14,8 @@
 (defun load-alternate-ometa ()
   (load-parser "ometa-parser1.lisp"))
 
-(defun load-parser (file-name)
-  (let ((*package* (find-package 'ometa)))
+(defun load-parser (file-name &key (package :ometa-output-package))
+  (let ((*package* (find-package package)))
     (declare (optimize (speed 0) (safety 3) (debug 3)))
     (load (compile-file "ometa.lisp"))
     (load (compile-file file-name))
@@ -27,34 +27,37 @@
   (load-alternate-ometa))
 
 (defun parse-grammar (file-name)
-    (let ((ometa (make-instance 'ometa-parser)))
-        (parse-input-file ometa file-name)))
+  (let ((ometa (make-instance 'ometa-parser)))
+    (parse-input-file ometa file-name)))
 
-(defmacro with-compiled-forms (class-name grammar-file-name action &optional initial-action)
-  `(let ((*ometa-compiler-target* ,class-name))
-     (let ((forms (mapcar #'ometa-compile (mapcar #'ometa-optimize (cdddr (parse-grammar ,grammar-file-name))))))
+(defmacro with-compiled-forms (class-name grammar-file-name package action &optional initial-action)
+  `(let ((*ometa-compiler-target* ,class-name)
+         (*package* (find-package ,package)))
+     (let ((forms (mapcar (lambda (form) (ometa-compile form *package*)) (mapcar #'ometa-optimize (cdddr (parse-grammar ,grammar-file-name))))))
        ,@(when initial-action
                `((when forms
                    (funcall ,initial-action))))
        (loop for form in forms
           do (funcall ,action form)))))
 
-(defun make-parser (class-name grammar-file-name parser-file-name &key optimize-debug optimize-speed)
-  (let ((quality (cond ((and optimize-debug optimize-speed)
+(defun make-parser (class-name grammar-file-name parser-file-name &key (package :ometa-output-package) optimize-debug optimize-speed)
+  (let ((package (find-package package))
+        (quality (cond ((and optimize-debug optimize-speed)
                         (error "Conflicting optimization requirements."))
                        (optimize-debug '(debug safety))
                        (optimize-speed '(speed))
                        (t              nil))))
     (with-open-file (stream parser-file-name :direction :output :if-exists :supersede)
-      (with-compiled-forms class-name grammar-file-name 
+      (with-compiled-forms class-name grammar-file-name package
                            (lambda (form) 
                              (format stream "~%") 
                              (let ((*print-readably* t))
                                (prin1 form stream)))
                            (lambda ()
+                             (format stream "~S~%" `(common-lisp:in-package ,(package-name package)))
                              (when quality
                                (write `(declaim (optimize ,@quality)) :stream stream)))))))
 
-(defun load-grammar (class-name grammar-file-name)
-    (with-compiled-forms class-name grammar-file-name
+(defun load-grammar (class-name grammar-file-name &optional (package (find-package :ometa-output-package)))
+    (with-compiled-forms class-name grammar-file-name package
       #'eval))
--- package.lisp
+++ package.lisp
@@ -1,5 +1,6 @@
 (defpackage #:ometa
   (:use :cl)
+  (:shadow #:o #:v #:x #:xs #:arg #:str #:nullarg)
   (:export
    #:ometa
    #:ometa-parser
@@ -9,18 +10,36 @@
    #:ometa-input-stream
    #:input-region
    #:load-grammar
+   ;; lexical category support
+   #:*posn*
+   #:ometa-apply
    ;; parser invocation
    #:parse-input-file
    #:parse-input-string
-   ;; parsers
-   #:letter
-   #:digit
-   #:anything
+   ;; parsers: ometa
+   #:empty
+   #:end
    #:exactly
    #:ochar
    #:ospace
    #:spaces
+   #:letter
+   #:digit
+   ;; parsers: ometa-prim
+   #:grammar
+   #:oand
+   #:oor
+   #:omany
+   #:omany1
+   #:seq
+   #:opred
+   #:onot
+   #:anything
+   #:form
    #:token
+   #:stringquote
+   #:firstandrest
+   #:listof
    ;; trace control
    #:*trace*
    #:*ignore-count*
@@ -30,3 +49,34 @@
    #:token-success-hook
    ))
 
+(defpackage #:ometa-output-package
+  (:use :common-lisp :ometa)
+  (:import-from #:ometa #:o #:v #:x #:xs #:arg #:str #:nullarg)
+  (:export
+   #:ometa #:o #:v #:x #:xs #:arg #:str #:nullarg
+   #:ometa-apply
+   ;; parsers: ometa
+   #:empty
+   #:end
+   #:exactly
+   #:ochar
+   #:ospace
+   #:spaces
+   #:letter
+   #:digit
+   ;; parsers: ometa-prim
+   #:grammar
+   #:oand
+   #:oor
+   #:omany
+   #:omany1
+   #:seq
+   #:opred
+   #:onot
+   #:anything
+   #:form
+   #:token
+   #:stringquote
+   #:firstandrest
+   #:listof
+   ))
